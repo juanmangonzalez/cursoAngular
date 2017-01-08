4d72da2af92914211afd1e7e8fecb17c8ea1b65e
jusnmsngonzalez <juanmangonzalez@gmail.com>
2017-01-08 20:50:14 -0300

    comentario2

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = [' $scope'];
-function LunchCheckController ($scope) {
 
-});
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope) {
+$scope.list = "";
+$scope.result = "";
+
+  $scope.checkIsEnough = function() {
+    var count = 0;
+    var meals = $scope.list.split(',');
+    for (var i = 0; i < meals.length; i++) {
+        if(meals[i]!=undefined && meals[i].trim()!="") {
+          count++;
+        }
+    }
+
+    $scope.result = count>3?"mucho":"poco";
+
+  };
+
+}
 
 })();
